56a21e642d7d7c268a20605fef02a5a6b12b959a
JuanDavidPena <j_david0915@hotmail.com>
2021-07-29 01:06:28 +0000

    [modify] more edge cases

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,17 +7,19 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re == 0)
+	if (c.re == 0 && c.im == 0)
 		printf("%d", 0);
 	if (c.re != 0)
 		printf("%g", c.re);
-	if ((c.re != 0 || c.re == 0) && c.im > 0)
+	if ((c.re != 0) && c.im > 0)
 		printf(" + ");
-	else if ((c.re != 0 || c.re == 0) && c.im < 0)
+	else if ((c.re != 0) && c.im < 0)
 		printf(" - ");
 	if (c.im > 0)
 		printf("%gi", c.im);
-	else if (c.im < 0)
+	else if (c.im < 0 && c.re != 0)
 		printf("%gi", c.im * -1);
+	else if (c.im < 0 && c.re == 0)
+		printf("%gi", c.im);
 	printf("\n");
 }
