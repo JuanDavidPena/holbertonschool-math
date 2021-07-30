c346e1ceab3508fa8bb87c61c46119db67af9ead
JuanDavidPena <j_david0915@hotmail.com>
2021-07-30 15:54:13 +0000

    [modify] 0-display.c task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,23 +9,28 @@ void display_complex_number(complex c)
 {
 	if (c.re == 0 && c.im == 0)
 		printf("%d", 0);
-	if (c.re != 0)
-		printf("%g", c.re);
-	if ((c.re != 0) && c.im > 0)
-		printf(" + ");
-	else if ((c.re != 0) && c.im < 0)
-		printf(" - ");
-	if (c.im > 0 && c.im != 1)
-		printf("%gi", c.im);
-	else if (c.im == 1)
-		printf("i");
-	else if (c.im < 0 && c.re != 0 && c.im != -1)
-		printf("%gi", c.im * -1);
-	else if (c.im < 0 && c.re != 0 && c.im == -1)
-		printf("i");
-	else if (c.im < 0 && c.re == 0 && c.im != -1)
-		printf("%gi", c.im);
-	else if (c.im < 0 && c.re == 0 && c.im == -1)
-		printf("-i");
+	else
+	{
+		if (c.re == 0 && c.im != 0)
+			printf("%d", 0);
+		if (c.re != 0)
+			printf("%g", c.re);
+		if (c.im > 0)
+			printf(" + ");
+		else if (c.im < 0)
+			printf(" - ");
+		if (c.im > 0 && c.im != 1)
+			printf("%gi", c.im);
+		else if (c.im == 1)
+			printf("i");
+		else if (c.im < 0 && c.re != 0 && c.im != -1)
+			printf("%gi", c.im * -1);
+		else if (c.im < 0 && c.re != 0 && c.im == -1)
+			printf("i");
+		else if (c.im < 0 && c.re == 0 && c.im != -1)
+			printf("%gi", c.im);
+		else if (c.im < 0 && c.re == 0 && c.im == -1)
+			printf("i");
+	}
 	printf("\n");
 }
